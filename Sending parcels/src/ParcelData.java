54dca5607450e93425b7043d65c45448b538f7a6
dmitryvlasov <TuBu2007@yandex.ru>
2026-04-30 15:08:39 +0200

    new

---
+++ Sending parcels/src/ParcelData.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+class ParcelData {
+    private String description;
+    private int weight;
+    private String deliveryAddress;
+    private int sendDay;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public boolean setWeight(int weight) {
+        if (weight > 0) {
+            this.weight = weight;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public int getSendDay() {
+        return sendDay;
+    }
+
+    public boolean setSendDay(int sendDay) {
+        if (sendDay >= 1 && sendDay <= 30) {
+            this.sendDay = sendDay;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ Sending parcels/src/StandardParcel.java
@@ -0,0 +1,13 @@
+public class StandardParcel extends Parcel {
+
+    public StandardParcel(String description, int weight, String deliveryAddress, int sendDay) {
+        super(description, weight, deliveryAddress, sendDay);
+    }
+
+    @Override
+    public String toString() {
+        return  "Стандартная посылка. " +
+                "Описание: " + description + ',' + " Вес посылки: " + weight + " Адрес доставки: " + deliveryAddress +
+                " День доставки: " + sendDay;
+    }
+}
